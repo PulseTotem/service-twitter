ce768152a4f1f267ca21b8c23a4928824e6d112c
Simon Urli <simon.urli@gmail.com>
2014-10-19 18:38:39 +0200

    Try something to get a tweet and show it.

--- scripts/Twitter.ts
+++ scripts/Twitter.ts
@@ -11,4 +11,29 @@
 var http = require("http");
 var express = require("express");
 var sio = require("socket.io");
+var twitterClient = require('twitter');
+var util = require('util');
 
+class Twitter {
+	run() {
+		/*var listeningPort = process.env.PORT_TWITTER || 4001;
+
+		var app = express();
+		var httpServer = http.createServer(app);
+		var io = sio.listen(httpServer);
+       */
+		var twit = new twitterClient({
+			consumer_key: 'STATE YOUR NAME',
+			consumer_secret: 'STATE YOUR NAME',
+			access_token_key: 'STATE YOUR NAME',
+			access_token_secret: 'STATE YOUR NAME'
+		});
+
+		twit.get('/statuses/show/27593302936.json', {include_entities:true}, function(data) {
+			console.log(util.inspect(data));
+		});
+	}
+}
+
+var twitter = new Twitter();
+twitter.run();
