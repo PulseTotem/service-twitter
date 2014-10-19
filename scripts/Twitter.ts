eae61b0594fe9b74e50bafbf593ab9711fe453d1
Simon Urli <simon.urli@gmail.com>
2014-10-19 18:12:37 +0200

    Start a class

---
+++ scripts/Twitter.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Simon Urli <simon@the6thscreen.fr>
+ */
+
+/// <reference path="../t6s-core/core-backend/libsdef/node.d.ts" />
+/// <reference path="../t6s-core/core-backend/libsdef/express.d.ts" />
+/// <reference path="../t6s-core/core-backend/libsdef/socket.io-0.9.10.d.ts" />
+
+/// <reference path="../t6s-core/core-backend/scripts/Logger.ts" />
+
+var http = require("http");
+var express = require("express");
+var sio = require("socket.io");
+
