1888e9fef051709d1b57f3b7b32a754b74b4841e
Simon Urli <simon.urli@gmail.com>
2015-04-24 17:10:11 +0200

    SERVICES-24 Start to work on a source album from twitter.

--- scripts/TwitterNamespaceManager.ts
+++ scripts/TwitterNamespaceManager.ts
@@ -36,6 +36,65 @@ class TwitterNamespaceManager extends SourceNamespaceManager {
         this.addListenerToSocket('LastTweetsFromSearch', this.retrieveLastTweetsFromSearch);
     }
 
+	retrieveTwitterUser(item : any) : User {
+		var twittos = item.user;
+		var owner : User = new User(twittos.id_str, 0, new Date(twittos.created_at), new Date());
+		owner.setUsername(twittos.screen_name);
+		owner.setRealname(twittos.name);
+		var loc : string = "Unknown";
+		if(twittos.location != null) {
+			loc = twittos.location;
+		}
+		owner.setLocation(loc);
+		owner.setProfilPicture(twittos.profile_image_url);
+
+		return owner;
+	}
+
+	retrievePictureEntity(media : any) : Picture {
+		var picture : Picture = new Picture(media.id_str, 0, new Date(), new Date());
+		picture.setTitle("");
+		picture.setDescription("");
+
+		var pictUrl_original : PictureURL = new PictureURL(media.id_str+"_original");
+		pictUrl_original.setURL(media.media_url);
+		pictUrl_original.setWidth(media.sizes.medium.w);
+		pictUrl_original.setHeight(media.sizes.medium.h);
+
+		picture.setOriginal(pictUrl_original);
+
+		var pictUrl_small : PictureURL = new PictureURL(media.id_str+"_small");
+		pictUrl_small.setURL(media.media_url + ":small");
+		pictUrl_small.setWidth(media.sizes.small.w);
+		pictUrl_small.setHeight(media.sizes.small.h);
+
+		picture.setSmall(pictUrl_small);
+
+		var pictUrl_medium : PictureURL = new PictureURL(media.id_str+"_medium");
+		pictUrl_medium.setURL(media.media_url);
+		pictUrl_medium.setWidth(media.sizes.medium.w);
+		pictUrl_medium.setHeight(media.sizes.medium.h);
+
+		picture.setMedium(pictUrl_medium);
+
+		var pictUrl_large : PictureURL = new PictureURL(media.id_str+"_large");
+		pictUrl_large.setURL(media.media_url + ":large");
+		pictUrl_large.setWidth(media.sizes.large.w);
+		pictUrl_large.setHeight(media.sizes.large.h);
+
+		picture.setLarge(pictUrl_large);
+
+		var pictUrl_thumb : PictureURL = new PictureURL(media.id_str+"_thumb");
+		pictUrl_thumb.setURL(media.media_url + ":thumb");
+		pictUrl_thumb.setWidth(media.sizes.thumb.w);
+		pictUrl_thumb.setHeight(media.sizes.thumb.h);
+
+		picture.setThumb(pictUrl_thumb);
+
+		picture.setOrientation("0");
+		return picture;
+	}
+
     /**
      * Retrieve last tweets from search and return the tweets in "TweetList" format.
      *
@@ -70,16 +129,7 @@ class TwitterNamespaceManager extends SourceNamespaceManager {
 
 					var tweet:Tweet = new Tweet(item.id_str, 0, new Date(item.created_at), new Date(), 10000);
 
-					var twittos = item.user;
-					var owner : User = new User(twittos.id_str, 0, new Date(twittos.created_at), new Date());
-					owner.setUsername(twittos.screen_name);
-					owner.setRealname(twittos.name);
-					var loc : string = "Unknown";
-					if(twittos.location != null) {
-						loc = twittos.location;
-					}
-					owner.setLocation(loc);
-					owner.setProfilPicture(twittos.profile_image_url);
+					var owner : User = self.retrieveTwitterUser(item);
 
 					tweet.setOwner(owner);
 					tweet.setMessage(item.text);
@@ -104,46 +154,7 @@ class TwitterNamespaceManager extends SourceNamespaceManager {
 					if(typeof(item.entities) != "undefined" && typeof(item.entities.media) != "undefined") {
 						item.entities.media.forEach(function (media : any) {
 							if (media.type == "photo") {
-								var picture : Picture = new Picture(media.id_str, 0, new Date(), new Date());
-								picture.setTitle("");
-								picture.setDescription("");
-
-								var pictUrl_original : PictureURL = new PictureURL(media.id_str+"_original");
-								pictUrl_original.setURL(media.media_url);
-								pictUrl_original.setWidth(media.sizes.medium.w);
-								pictUrl_original.setHeight(media.sizes.medium.h);
-
-								picture.setOriginal(pictUrl_original);
-
-								var pictUrl_small : PictureURL = new PictureURL(media.id_str+"_small");
-								pictUrl_small.setURL(media.media_url + ":small");
-								pictUrl_small.setWidth(media.sizes.small.w);
-								pictUrl_small.setHeight(media.sizes.small.h);
-
-								picture.setSmall(pictUrl_small);
-
-								var pictUrl_medium : PictureURL = new PictureURL(media.id_str+"_medium");
-								pictUrl_medium.setURL(media.media_url);
-								pictUrl_medium.setWidth(media.sizes.medium.w);
-								pictUrl_medium.setHeight(media.sizes.medium.h);
-
-								picture.setMedium(pictUrl_medium);
-
-								var pictUrl_large : PictureURL = new PictureURL(media.id_str+"_large");
-								pictUrl_large.setURL(media.media_url + ":large");
-								pictUrl_large.setWidth(media.sizes.large.w);
-								pictUrl_large.setHeight(media.sizes.large.h);
-
-								picture.setLarge(pictUrl_large);
-
-								var pictUrl_thumb : PictureURL = new PictureURL(media.id_str+"_thumb");
-								pictUrl_thumb.setURL(media.media_url + ":thumb");
-								pictUrl_thumb.setWidth(media.sizes.thumb.w);
-								pictUrl_thumb.setHeight(media.sizes.thumb.h);
-
-								picture.setThumb(pictUrl_thumb);
-
-								picture.setOrientation("0");
+								var picture : Picture = self.retrievePictureEntity(media);
 
 								tweet.getHashtags().forEach(function(tag) {
 									picture.addTag(tag);
@@ -172,4 +183,30 @@ class TwitterNamespaceManager extends SourceNamespaceManager {
 
 		self.manageOAuth('twitter', params.oauthKey, success, fail);
     }
+
+	/*
+	retrievePictureAlbumFromTwitterSearch(params : any, self : TwitterNamespaceManager = null) {
+
+		if(self == null) {
+			self = this;
+		}
+
+		Logger.debug("PictureAlbumFromTwitterSearch Action with params :");
+		Logger.debug(params);
+
+		var fail = function(error) {
+			if(error) {
+				Logger.error(error);
+			}
+		};
+
+		var success = function (info) {
+
+			var searchUrl = '/1.1/search/tweets.json?q=' + params.SearchQuery + '&count=' + params.Limit + '&result_type=recent';
+			oauthActions.get(searchUrl, successSearch, fail);
+		};
+
+		self.manageOAuth('twitter', params.oauthKey, success, fail);
+	}
+	* /
 }
