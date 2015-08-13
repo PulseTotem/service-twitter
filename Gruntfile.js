9c4b5141a9d9b8a0dc3288298e9283655774da4d
Christian Brel <ch.brel@gmail.com>
2015-08-13 18:09:01 +0200

    refs SERVICES-87 : Updated build task to copy package.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,17 +65,6 @@ module.exports = function (grunt) {
                     'devDependencies',
                     'overrides'
                 ]
-            },
-            packageHeroku: {
-              src: ['t6s-core/core-backend/package.json'],
-              dest: 'heroku/package.json',
-              fields: [
-                'name',
-                'version',
-                'dependencies',
-                'devDependencies',
-                'overrides'
-              ]
             }
         },
 // ---------------------------------------------
@@ -93,6 +82,11 @@ module.exports = function (grunt) {
             buildPackageReinit: {
                 files: 	[{'package.json': 'package-bak.json'}]
             },
+
+            keepPackage: {
+              files: 	[{'build/package.json': 'package.json'}]
+            },
+
             heroku: {
               files: 	[{expand: true, cwd: 'build', src: ['**'], dest: 'heroku'}]
             },
@@ -228,7 +222,7 @@ module.exports = function (grunt) {
     grunt.registerTask('build', function () {
         grunt.task.run(['clean:package', 'clean:build']);
 
-        grunt.task.run(['env:build','update_json:packageBuild', 'copy:buildPackageBak', 'copy:buildPackageReplace', 'npm-install', 'copy:buildPackageReinit', 'typescript:build', 'clean:package']);
+        grunt.task.run(['env:build','update_json:packageBuild', 'copy:buildPackageBak', 'copy:buildPackageReplace', 'npm-install', 'copy:keepPackage', 'copy:buildPackageReinit', 'typescript:build', 'clean:package']);
     });
 
     grunt.registerTask('heroku', function () {
